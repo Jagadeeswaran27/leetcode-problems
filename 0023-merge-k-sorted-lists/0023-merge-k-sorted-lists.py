b8dcf6884446774f729a7eeed3e760bdb5a22d6e
Jagadeeswaran M <118825972+Jagadeeswaran27@users.noreply.github.com>
2026-02-24 22:23:32 +0530

    Time: 11 ms (59.32%), Space: 22.5 MB (80.64%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Btute Force
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         nodes = []
+#         for list in lists:
+#             while list:
+#                 nodes.append(list.val)
+#                 list = list.next
+
+#         nodes.sort()
+#         res = ListNode(0)
+#         curr = res
+
+#         for node in nodes:
+#             curr.next = ListNode(node)
+#             curr = curr.next
+        
+#         return res.next
+
+
+      
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0:
+            return None
+        
+        while len(lists)>1:
+            mergedLists = []
+            for i in range(0,len(lists),2):
+                l1 = lists[i]
+                l2 = lists[i+1] if i+1<len(lists) else None
+                mergedLists.append(self.mergeList(l1,l2))
+            lists = mergedLists
+        
+        return lists[0]
+    
+    def mergeList(self,l1,l2):
+        dummy = ListNode(0)
+        curr = dummy
+
+        while l1 and l2:
+            if l1.val<l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        
+        curr.next = l1 or l2
+        
+        return dummy.next
