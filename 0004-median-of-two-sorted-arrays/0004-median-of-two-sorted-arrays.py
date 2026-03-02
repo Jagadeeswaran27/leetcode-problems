42b9f5253f5fecd11e37e1d4f6d106103f6c6d3f
Jagadeeswaran M <118825972+Jagadeeswaran27@users.noreply.github.com>
2026-03-02 21:26:52 +0530

    Time: 4 ms (30.06%), Space: 19.5 MB (72.32%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,27 +1,58 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        i=j=0
-        new_nums = []
 
-        while i<len(nums1) and j<len(nums2):
-            if nums1[i]<nums2[j]:
-                new_nums.append(nums1[i])
-                i+=1
+        A,B = nums1,nums2
+
+        total = len(nums1)+len(nums2)
+        half = total//2
+
+        if len(B)<len(A):
+            A,B = B,A
+        
+        l,r = 0,len(A)-1
+
+        while True:
+            i = (l+r)//2
+            j = half-i-2
+
+            Aleft = A[i] if i>=0 else float("-infinity")
+            Aright = A[i+1] if (i+1)<len(A) else float("infinity")
+            Bleft = B[j] if j>=0 else float("-infinity")
+            Bright = B[j+1] if (j+1)<len(B) else float("infinity")
+
+            if Aleft<=Bright and Bleft<=Aright:
+                if total%2:
+                    return min(Aright,Bright)
+                return (max(Aleft,Bleft)+min(Aright,Bright))/2
+            elif Aleft>Bright:
+                r = i-1
             else:
-                new_nums.append(nums2[j])
-                j+=1
+                l = i+1
         
-        while i<len(nums1):
-            new_nums.append(nums1[i])
-            i+=1
-        
-        while j<len(nums2):
-            new_nums.append(nums2[j])
-            j+=1
-        
-        n = len(new_nums)
 
-        if n%2 == 0:
-            return (new_nums[(n//2)-1]+new_nums[n//2])/2
-        else:
-            return new_nums[n//2]
+        # Brute force
+        # i=j=0
+        # new_nums = []
+
+        # while i<len(nums1) and j<len(nums2):
+        #     if nums1[i]<nums2[j]:
+        #         new_nums.append(nums1[i])
+        #         i+=1
+        #     else:
+        #         new_nums.append(nums2[j])
+        #         j+=1
+        
+        # while i<len(nums1):
+        #     new_nums.append(nums1[i])
+        #     i+=1
+        
+        # while j<len(nums2):
+        #     new_nums.append(nums2[j])
+        #     j+=1
+        
+        # n = len(new_nums)
+
+        # if n%2 == 0:
+        #     return (new_nums[(n//2)-1]+new_nums[n//2])/2
+        # else:
+        #     return new_nums[n//2]
