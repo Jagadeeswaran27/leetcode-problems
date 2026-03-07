117a7f568f9b939c274c48e6148734890f2d58ee
Jagadeeswaran M <118825972+Jagadeeswaran27@users.noreply.github.com>
2026-03-07 20:25:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -15,11 +15,21 @@ class Solution:
                     if 0<=nr<ROWS and 0<=nc<COLS and grid[nr][nc] == "1":
                         queue.append((nr,nc))
                         grid[nr][nc] = "0"
+        
+        def dfs(row,col):
+            if 0<=row<ROWS and 0<=col<COLS and grid[row][col] == "1":
+                grid[row][col] = "0"
+                dfs(row,col+1)
+                dfs(row,col-1)
+                dfs(row+1,col)
+                dfs(row-1,col)
+        
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
                     count+=1
-                    bfs(r,c)
+                    # bfs(r,c)
+                    dfs(r,c)
         return count
         
